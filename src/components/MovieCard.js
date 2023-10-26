cbfe1f846b0cdadf5359c3e53ea9844bcd32a6ff
GreenCatInForest <event2016.2017@gmail.com>
2023-10-26 17:45:12 +0100

    style movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,16 @@
 export const MovieCard = () => {
   return (
-    <div class="card movie-card">
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
+    <div class="card movie-card m-3">
+      <img src="..." class="card-img-top" alt="movie banner" />
+      <div class="card-body text-center">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+        <button type="button" class="btn btn-primary">
+          Add to Fav
+        </button>
       </div>
     </div>
   );
